1b625b00f56aa86e22916423fcf3b7d5c6d7211a
Jack Cherng <jfcherng@gmail.com>
2026-02-08 03:32:46 +0800

    refactor: move server resource management to VersionManager
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+import re
+
+import sublime
 from LSP.plugin import register_plugin, unregister_plugin
 
 from .client import LspRuffPlugin
+from .constants import PACKAGE_NAME
+from .version_manager import version_manager
 
 __all__ = (
     # ST: core
@@ -17,7 +22,21 @@ def plugin_loaded() -> None:
     """Executed when this plugin is loaded."""
     register_plugin(LspRuffPlugin)
 
+    version_manager.client_cls = LspRuffPlugin
+    version_manager.server_version = _get_server_version()
+
 
 def plugin_unloaded() -> None:
     """Executed when this plugin is unloaded."""
     unregister_plugin(LspRuffPlugin)
+
+
+def _get_server_version() -> str:
+    """The server version without a "v" prefix."""
+    if m := re.search(
+        r"^ruff==(.+)",
+        sublime.load_resource(f"Packages/{PACKAGE_NAME}/requirements.txt"),
+        re.MULTILINE,
+    ):
+        return m.group(1).strip()
+    raise ValueError("Failed to parse server version from requirements.txt")
--- plugin/client.py
+++ plugin/client.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
 
-import io
 import os
-from pathlib import Path
 from typing import Any
 
 import sublime
 from LSP.plugin import AbstractPlugin, DottedDict
 
 from .constants import PACKAGE_NAME
-from .log import log_info, log_warning
+from .log import log_warning
 from .template import load_string_template
-from .utils import decompress_buffer, rmtree_ex, sha256sum, simple_urlopen
 from .version_manager import version_manager
 
 
@@ -29,30 +26,16 @@ class LspRuffPlugin(AbstractPlugin):
     @classmethod
     def additional_variables(cls) -> dict[str, str] | None:
         return {
-            "server_path": str(cls.server_path()),
+            "server_path": str(version_manager.server_path),
         }
 
     @classmethod
     def needs_update_or_installation(cls) -> bool:
-        return not cls.server_path().is_file()
+        return not version_manager.is_installed
 
     @classmethod
     def install_or_update(cls) -> None:
-        rmtree_ex(cls.plugin_storage_dir(), ignore_errors=True)
-
-        log_info(f"Downloading server tarball: {version_manager.server_download_url}")
-        data = simple_urlopen(version_manager.server_download_url)
-
-        hash_actual = sha256sum(data)
-        hash_golden = simple_urlopen(version_manager.server_download_hash_url).decode().partition(" ")[0]
-        if hash_actual != hash_golden:
-            raise ValueError(f"Mismatched downloaded file hash: {hash_actual} != {hash_golden}")
-
-        decompress_buffer(
-            io.BytesIO(data),
-            filename=version_manager.THIS_TARBALL_NAME,
-            dst_dir=cls.versioned_server_dir(),
-        )
+        version_manager.install_server()
 
     @classmethod
     def should_ignore(cls, view: sublime.View) -> bool:
@@ -75,21 +58,6 @@ class LspRuffPlugin(AbstractPlugin):
     # -------------- #
     # custom methods #
     # -------------- #
-
-    @classmethod
-    def plugin_storage_dir(cls) -> Path:
-        """The storage directory for this plugin."""
-        return Path(cls.storage_path()) / PACKAGE_NAME
-
-    @classmethod
-    def versioned_server_dir(cls) -> Path:
-        """The directory specific to the current server version."""
-        return cls.plugin_storage_dir() / f"v{version_manager.server_version}"
-
-    @classmethod
-    def server_path(cls) -> Path:
-        """The path of the language server binary."""
-        return cls.versioned_server_dir() / version_manager.THIS_TARBALL_BIN_PATH
 
     def update_status_bar_text(self, extra_variables: dict[str, Any] | None = None) -> None:
         if not (session := self.weaksession()):
