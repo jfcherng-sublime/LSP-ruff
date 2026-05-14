ae0cc94dc1d6d1c40fdb3a2cd65c233d4ffad9c6
Jack Cherng <jfcherng@gmail.com>
2026-05-14 23:40:45 +0800

    chore: ruff 0.15.13
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/constants.py
+++ plugin/constants.py
@@ -6,5 +6,5 @@ PACKAGE_NAME = __package__.partition(".")[0]
 
 PLATFORM_ARCH = f"{sublime.platform()}_{sublime.arch()}"
 
-SERVER_VERSION = "0.15.12"
+SERVER_VERSION = "0.15.13"
 """The server tag. You can find them on https://github.com/astral-sh/ruff/releases"""
